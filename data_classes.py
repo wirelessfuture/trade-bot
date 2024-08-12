553081fe279b629e611a48bb4d9722c52896f9c7
wirelessfuture <wirelessfuture2000@gmail.com>
2024-08-13 00:25:01 +0200

    Convert Candles to KLines in Binance data format

--- data_classes.py
+++ data_classes.py
@@ -1,65 +1,86 @@
 import datetime
 from dataclasses import dataclass, field
-from typing import Dict, Any, List
+from typing import List, Any, Dict
 
-import pandas as pd
+from pandas import Series, DataFrame
 
 
 @dataclass
-class CandleSeries:
-    close: float
-    ctm: datetime
-    ctmString: str
+class KLine:
+    open_time: datetime.datetime
+    open: float
     high: float
     low: float
-    open: float
-    vol: float
+    close: float
+    volume: float
+    close_time: datetime.datetime
+    quote_asset_volume: float
+    number_of_trades: int
+    taker_buy_base_asset_volume: float
+    taker_buy_quote_asset_volume: float
 
-    def __init__(self, entry: Dict[str, Any], digits: int) -> None:
-        scaling_factor = 10**digits
-        self.open = entry["open"] / scaling_factor
-        self.close = self.open + (entry["close"] / scaling_factor)
-        self.high = self.open + (entry["high"] / scaling_factor)
-        self.low = self.open + (entry["low"] / scaling_factor)
-        self.ctm = entry["ctm"]
-        self.ctmString = entry["ctmString"]
-        self.vol = entry["vol"]
+    def __init__(self, entry: List[Any]) -> None:
+        self.open_time = datetime.datetime.fromtimestamp(entry[0] / 1000.0)
+        self.open = float(entry[1])
+        self.high = float(entry[2])
+        self.low = float(entry[3])
+        self.close = float(entry[4])
+        self.volume = float(entry[5])
+        self.close_time = datetime.datetime.fromtimestamp(entry[6] / 1000.0)
+        self.quote_asset_volume = float(entry[7])
+        self.number_of_trades = int(entry[8])
+        self.taker_buy_base_asset_volume = float(entry[9])
+        self.taker_buy_quote_asset_volume = float(entry[10])
 
-    def to_series(self) -> pd.Series:
+    def to_json(self) -> Dict[str, Any]:
         data: Dict[str, Any] = {
-            "ctm": self.ctm,
-            "ctmString": self.ctmString,
+            "open_time": self.open_time,
             "open": self.open,
             "high": self.high,
             "low": self.low,
             "close": self.close,
-            "vol": self.vol,
+            "volume": self.volume,
+            "close_time": self.close_time,
+            "quote_asset_volume": self.quote_asset_volume,
+            "number_of_trades": self.number_of_trades,
+            "taker_buy_base_asset_volume": self.taker_buy_base_asset_volume,
+            "taker_buy_quote_asset_volume": self.taker_buy_quote_asset_volume,
         }
-        return pd.Series(data)
+        return data
+
+    def to_series(self) -> Series:
+        return Series(self.to_json())
 
 
 @dataclass
-class CandleFrame:
-    digits: int
-    candles: List[CandleSeries] = field(default_factory=list)
+class KLines:
+    klines: List[KLine] = field(default_factory=list)
 
-    def __init__(self, data: Dict[str, Any]) -> None:
-        self.digits = data["returnData"]["digits"]
-        rate_infos: Dict[str, Any] = data["returnData"]["rateInfos"]
-        self.candles = [CandleSeries(entry, self.digits) for entry in rate_infos]
+    def __init__(self, data: List[List[Any]]) -> None:
+        self.klines = [KLine(entry) for entry in data]
 
-    def to_dataframe(self) -> pd.DataFrame:
+    def to_json(self) -> Dict[str, Any]:
         data: Dict[str, Any] = {
-            "ctm": [candle.ctm for candle in self.candles],
-            "ctmString": [candle.ctmString for candle in self.candles],
-            "open": [candle.open for candle in self.candles],
-            "high": [candle.high for candle in self.candles],
-            "low": [candle.low for candle in self.candles],
-            "close": [candle.close for candle in self.candles],
-            "vol": [candle.vol for candle in self.candles],
+            "open_time": [kline.open_time for kline in self.klines],
+            "open": [kline.open for kline in self.klines],
+            "high": [kline.high for kline in self.klines],
+            "low": [kline.low for kline in self.klines],
+            "close": [kline.close for kline in self.klines],
+            "volume": [kline.volume for kline in self.klines],
+            "close_time": [kline.close_time for kline in self.klines],
+            "quote_asset_volume": [kline.quote_asset_volume for kline in self.klines],
+            "number_of_trades": [kline.number_of_trades for kline in self.klines],
+            "taker_buy_base_asset_volume": [
+                kline.taker_buy_base_asset_volume for kline in self.klines
+            ],
+            "taker_buy_quote_asset_volume": [
+                kline.taker_buy_quote_asset_volume for kline in self.klines
+            ],
         }
-        df = pd.DataFrame(data)
-        df["ctm"] = pd.to_datetime(df["ctm"], unit="ms")
-        df.set_index("ctm", inplace=True)
+        return data
 
-        return pd.DataFrame(df)
+    def to_dataframe(self) -> DataFrame:
+        df = DataFrame(self.to_json())
+        df.set_index("open_time", inplace=True)
+
+        return df
