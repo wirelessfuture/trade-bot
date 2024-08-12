2230a2fc3564170b5d67b1f3c92136b1c89c4814
wirelessfuture <wirelessfuture2000@gmail.com>
2024-08-13 00:24:48 +0200

    Add Binance constants

--- constants.py
+++ constants.py
@@ -5,5 +5,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-XTB_USER = os.getenv("XTB_USER")
-XTB_PASSWORD = os.getenv("XTB_PASSWORD")
+# Key Constants
+BINANCE_API_KEY = os.getenv("BINANCE_API_KEY")
+BINANCE_API_SECRET = os.getenv("BINANCE_API_SECRET")
+BINANCE_TESTNET_API_KEY = os.getenv("BINANCE_TESTNET_API_KEY")
+BINANCE_TESTNET_API_SECRET = os.getenv("BINANCE_TESTNET_API_SECRET")
+
+# Currency Constants
+BINANCE_TRADE_CURRENCY = "USDC"
+
+# Endpoint Constants
+BINANCE_TESTNET_BASE_URL = "https://testnet.binance.vision"
+BINANCE_TESTNET_DATA_URL = "https://data-api.binance.vision"
