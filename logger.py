d62fc13bd058bb58ace9007c470f5e7f250f5a79
wirelessfuture <wirelessfuture2000@gmail.com>
2024-08-13 00:25:29 +0200

    Change logger name

--- logger.py
+++ logger.py
@@ -2,7 +2,7 @@ from coloredlogs import install
 from logging import getLogger, basicConfig, Logger
 
 
-logger: Logger = getLogger("jsonSocket")
+logger: Logger = getLogger("TradeBot")
 FORMAT: str = "[%(asctime)-15s][%(funcName)s:%(lineno)d] %(message)s"
 basicConfig(format=FORMAT)
 install(level="INFO")
