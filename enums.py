006044810d8520ae5a62f2ef9429516343321d89
wirelessfuture <wirelessfuture2000@gmail.com>
2024-08-13 00:25:10 +0200

    Convert enums to Binance Enums

--- enums.py
+++ enums.py
@@ -1,36 +1,53 @@
 from enum import Enum
 
 
-class Period(Enum):
-    PERIOD_M1 = 1
-    PERIOD_M5 = 5
-    PERIOD_M15 = 15
-    PERIOD_M30 = 30
-    PERIOD_H1 = 60
-    PERIOD_H4 = 240
-    PERIOD_D1 = 1440
-    PERIOD_W1 = 10080
-    PERIOD_MN1 = 43200
+class Interval(Enum):
+    SECOND_1 = "1s"
+    MINUTE_1 = "1m"
+    MINUTE_3 = "3m"
+    MINUTE_5 = "5m"
+    MINUTE_15 = "15m"
+    MINUTE_30 = "30m"
+    HOUR_1 = "1h"
+    HOUR_2 = "2h"
+    HOUR_4 = "4h"
+    HOUR_6 = "6h"
+    HOUR_8 = "8h"
+    HOUR_12 = "12h"
+    DAY_1 = "1d"
+    DAY_3 = "3d"
+    WEEK_1 = "1w"
+    MONTH_1 = "1M"
 
 
-class TradeCommand(Enum):
+class OrderStatus(Enum):
+    NEW = 0
+    PENDING_NEW = 1
+    PARTIALLY_FILLED = 2
+    FILLED = 3
+    CANCELLED = 4
+    PENDING_CANCEL = 5
+    REJECTED = 6
+    EXPIRED = 7
+    EXPIRED_IN_MATCH = 8
+
+
+class OrderType(Enum):
+    LIMIT = 0
+    MARKET = 1
+    STOP_LOSS = 2
+    STOP_LOSS_LIMIT = 3
+    TAKE_PROFIT = 4
+    TAKE_PROFIT_LIMIT = 5
+    LIMIT_MAKER = 6
+
+
+class OrderSide(Enum):
     BUY = 0
     SELL = 1
-    BUY_LIMIT = 2
-    SELL_LIMIT = 3
-    BUY_STOP = 4
-    SELL_STOP = 5
 
 
-class TradeType(Enum):
-    OPEN = 0
-    CLOSE = 2
-    MODIFY = 3
-    DELETE = 4
-
-
-class TradeStatus(Enum):
-    ERROR = 0
-    PENDING = 1
-    ACCEPTED = 3
-    REJECTED = 4
+class TimeInForce(Enum):
+    GTC = 0 # Good Til Cancelled
+    IOC = 1 # Immediate Or Cancel
+    FOK = 2 # Fill Or Kill
